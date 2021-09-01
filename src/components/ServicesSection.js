e413b1ea24a0b2c78070bd04581403705f8455d1
Your Name <leobarrientos02@gmail.com>
2021-09-01 03:38:39 -0400

    Create ServicesSection.js

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+// Import Icons
+import clock from '../img/clock.svg';
+import diaphragm from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+
+const ServicesSection = () =>{
+    return(
+        <div className="services">
+            <div className="description">
+                <h2>
+                    High <span>quality</span> services. 
+                </h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ServicesSection

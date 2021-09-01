2df0c5cb33f78e275608157f9af83fd03eeded98
Your Name <leobarrientos02@gmail.com>
2021-09-01 03:16:59 -0400

    AboutUs.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }

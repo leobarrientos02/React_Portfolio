320a05a3352547d1dd990dd48be9ba4ad527021b
Your Name <leobarrientos02@gmail.com>
2021-09-01 22:18:11 -0400

    FAQ Section

---
+++ src/components/FAQSection.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const FAQSection = () =>{
+    return(
+        <div className="FAQ">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>RANDOM</p>
+                    <p>
+                        RANDOM
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>RANDOM</p>
+                    <p>
+                        RANDOM
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>What Products do we Offer?</h4>
+                <div className="answer">
+                    <p>RANDOM</p>
+                    <p>
+                        RANDOM
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Daily Schedule?</h4>
+                <div className="answer">
+                    <p>RANDOM</p>
+                    <p>
+                        RANDOM
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQSection;

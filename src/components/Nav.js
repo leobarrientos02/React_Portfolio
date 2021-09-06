c34c3bc34ffa45bef502dafcb9637472d2f4d63d
Your Name <leobarrientos02@gmail.com>
2021-09-06 03:25:21 -0400

    developing the nav section

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>
+        <a href="#">Capture</a>
+        <ul>
+            <li>
+                <a href="#">1. About Us</a>
+            </li>
+            <li>
+                <a href="#">2. Our Work</a>
+            </li>
+            <li>
+                <a href="#">3. Contact Us</a>
+            </li>                        
+        </ul>
+      </h1>
+    </nav>
+  );
+};
+
+export default Nav;
